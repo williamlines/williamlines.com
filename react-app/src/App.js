d02d3a0eb656b0948bbe188eb22f3fff8d5ab3d2
William Lines <williamlines42@gmail.com>
2023-02-15 15:43:02 +0000

    reformatted to include new file structure and chakra

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <h1> William Lines </h1>
+    </ChakraProvider>
+  );
+}
+
+export default App;
